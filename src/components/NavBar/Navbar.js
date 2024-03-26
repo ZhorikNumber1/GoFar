d1f297c0be9e525f92b34a0e1c21f927fb4005fe
pasha123jarcov <109911382+ZhorikNumber1@users.noreply.github.com>
2024-03-27 02:40:57 +0300

    smtf

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Navbar.module.scss';  // Предположим, что вы используете CSS-modules
+import {Link} from 'react-router-dom';
+import styles from './Navbar.module.scss';
+import icons from './../../images/icon.png'
 
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
+            <div className={styles["navbar-brand"]}>
+                <div className={styles.icon}>
+                    <Link to="/"><img src={icons} alt="Logo"/></Link>
+                </div>
+            </div>
             <ul className={styles["navbar-nav"]}>
-                <li><Link to="/">Главная</Link></li>
+                <li><Link to="/features">Преимущества</Link></li>
+                <li><Link to="/services">Услуги</Link></li>
+                <li><Link to="/countries">Страны</Link></li>
                 <li><Link to="/about">О нас</Link></li>
-                <li><Link to="/contact">Контакты</Link></li>
+                <li><Link to="/reviews">Отзывы</Link></li>
             </ul>
+            <div className={styles.div_buttons}>
+                <button className={styles.button_school}>Языковая школа</button>
+                <button className={styles.button_cons}>Бесплатная консультация</button>
+            </div>
         </nav>
 
     );
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -7,22 +7,96 @@ const MainPage = () => {
     return (
         <div className={style.container}>
             <Navbar/>
-            <div className={style.header_container}>
-                <div className={style.interactive_container}>
-                    <img src={union} alt="Интерактивный Фон"/>
-                    <div className={style.interactive_content}>
-                        <h1>Поможем <strong>поступить</strong> в вуз мечты <strong>за границей</strong></h1>
-                        <div className={style.buttons}>
-                            <button>123</button>
-                            <button>321</button>
+            <div className={style.header_div}>
+                <div className={style.header_container}>
+                    <div className={style.interactive_container}>
+                        <img src={union} alt="Интерактивный Фон"/>
+                        <div className={style.interactive_content}>
+                            <h1>Поможем <strong>поступить</strong> в вуз мечты <strong>за границей</strong></h1>
+                            <div className={style.buttons}>
+                                <button className={style.btn_submit_app}>Оставить заявку</button>
+                                <button className={style.btn_more_info}>Больше о GoFar</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className={style.second_div}>
+                        <img src="путь_к_изображению" alt="Описание изображения"/>
+                        <div className={style.information}>
+                            <p className={style.text}>Помогаем поступить в вузы:</p>
+                            <a href="#" className={style.next}>&#8250;</a>                            <p
+                            className={style.text}>Австрия</p>
+                            <button className={style.button}>123</button>
                         </div>
                     </div>
-
-
                 </div>
+                <div className={style.cards_container}>
+                    <div className={style.card}>Бесплатное обучение</div>
+                    <div className={style.card}>Возможность по расчету</div>
+                    <div className={style.card}>Без ограничений по возрасту</div>
+                    <div className={style.card}>Обучение на английском</div>
+                    <div className={style.card}>Консультация экспертов GoFar</div>
+                    <div className={style.card}>Планове сопровождание</div>
+                </div>
+            </div>
+            <div className={style.workin_with}>
                 <div>
-                    123
+                    С кем мы работаем
                 </div>
+                <div className={style.with_gap}>
+                    <div className={style.with_inline}>
+                        <div className={style.bakalavr}>
+                            1
+                        </div>
+                        <div className={style.student}>
+                            2
+                        </div>
+                    </div>
+                    <div className={style.with_inline}>
+                        <div className={style.card_helpfull}>3</div>
+                        <div className={style.card_helpfull}>4</div>
+                        <div className={style.card_helpfull}>5</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={style.advantages_cards}>
+                <div className={style.advantages}></div>
+                <div className={style.stage}></div>
+                <div className={style.risks}></div>
+            </div>
+
+            <div>
+                локация
+            </div>
+
+            <div>
+                prises
+            </div>
+
+            <div>
+                comrads
+            </div>
+
+            <div>
+                resipients
+            </div>
+
+            <div>
+                kyda posrypayt
+            </div>
+
+            <div>
+                quastions
+            </div>
+
+            <div>
+                konsa
+            </div>
+
+            <div>
+                contacts
             </div>
         </div>
     );
