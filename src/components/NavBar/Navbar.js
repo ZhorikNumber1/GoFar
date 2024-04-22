e1e81f3829ffe811d3eaf05f851bd16ccb85a6f5
pasha123jarcov <109911382+ZhorikNumber1@users.noreply.github.com>
2024-04-22 15:09:03 +0300

    13

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import {Link} from 'react-router-dom';
 import styles from './Navbar.module.scss';
 import icons from './../../images/icon.png'
 
 const Navbar = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
     return (
-        <nav className={`${styles.navbar} ${styles.slideInFromUp}`}>`
+        <nav className={`${styles.navbar} ${styles.slideInFromUp}`}>
             <div className={styles["navbar-brand"]}>
                 <div className={styles.icon}>
                     <Link to="/"><img src={icons} alt="Logo"/></Link>
@@ -14,13 +20,25 @@ const Navbar = () => {
             <ul className={styles["navbar-nav"]}>
                 <li><Link to="/features">Преимущества</Link></li>
                 <li><Link to="/services">Услуги</Link></li>
-                <li><Link to="/countries">Страны</Link></li>
+                <li className={styles.dropdown}>
+                    <button className={styles.dropbtn} onClick={toggleDropdown}>
+                    Страны <i className="fa fa-caret-down"></i>
+                    </button>
+                {dropdownOpen && (
+                    <div className={`${styles.dropdownContent} ${dropdownOpen ? styles.active : ''}`}>
+                            <Link to="/reviews/USA">USA</Link>
+                            <Link to="/reviews/UK">UK</Link>
+                            <Link to="/reviews/Canada">Canada</Link>
+                    </div>
+                 )}
+                </li>
                 <li><Link to="/about">О нас</Link></li>
-                <li><Link to="/reviews">Отзывы</Link></li>
+                <li><Link to="/countries">Отзовы</Link></li>
+
             </ul>
             <div className={styles.div_buttons}>
-                <button className={styles.button_school}>Языковая школа</button>
-                <button className={styles.button_cons}>Бесплатная консультация</button>
+                <Link to="/school"><button className={styles.button_school}>Языковая школа</button></Link>
+                <Link to="/consultation"><button className={styles.button_cons}>Бесплатная консультация</button></Link>
             </div>
         </nav>
     );
